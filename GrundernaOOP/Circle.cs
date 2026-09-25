10153a623ef6d8a2a863fdeee1e174f033ce4a24
Svea Mermans <sveamermans@gmail.com>
2026-09-25 17:50:03 +0200

    Add project files.

---
+++ GrundernaOOP/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrundernaOOP
+{
+    internal class Circle //clas called Circle
+    {
+        private int _radius; //declaring the integer for the radius,
+                             //making the field private since the starting code said _radius
+
+        public Circle(int radius) //konstruktor
+        {
+            _radius = radius; //the int _radius will be the value added in the int radius in the brackets () by the user
+        }
+
+        public double GetArea() //method to calculate the circle area
+        {
+            return _radius * _radius * Math.PI; //returns the value of radius*radius*PI
+        }
+
+    }
+}
---
+++ GrundernaOOP/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GrundernaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a new object with the class Circle, adding an int in the brackets which will be the Radius
+            Circle circle1 = new Circle(5); 
+            Console.WriteLine("The area of the circle is: " + circle1.GetArea());
+
+            Circle circle2 = new Circle(6);
+            Console.WriteLine("The area of the circle is: " + circle2.GetArea());
+        }
+    }
+}
